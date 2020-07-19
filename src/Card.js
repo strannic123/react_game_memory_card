9452422e57045995c09e9e47da54adf0ded24abb
str <manager350@mail.ru>
2020-07-19 22:26:57 +0800

    added flip

--- src/Card.js
+++ src/Card.js
@@ -1,11 +1,37 @@
 import React, {Component} from 'react';
 import "./Card.css"
+import ReactCardFlip from 'react-card-flip';
 
 class Card extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isFlipped: false
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    this.setState(prevState => ({ isFlipped: !prevState.isFlipped }));
+  }
+
   render() {
     return (
       <div className="card">
-        <i className={`fa ${this.props.icon} fa-4x`}></i>
+        <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="vertical">
+          <div className="front-card">
+
+            <button onClick={this.handleClick}>Click to flip</button>
+          </div>
+
+          <div className="back-card">
+            <i className={`fa ${this.props.icon} fa-4x `}></i>
+            <button onClick={this.handleClick}>Click to back</button>
+
+          </div>
+        </ReactCardFlip>
+
       </div>
     );
   }
